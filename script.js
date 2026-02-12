f395c3791d43d32f23de8aa08ab2719f830f490a
ANDREWBORIS <seglanrew9@gmail.com>
2026-02-12 14:36:34 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const noBtn = document.getElementById("no");
+const yesBtn = document.getElementById("yes");
+const loveText = document.getElementById("loveText");
+
+let noScale = 1;
+let yesScale = 1;
+noBtn.addEventListener("mouseover", () => {
+
+  // Il fuit
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+
+  // Il rétrécit
+  noScale -= 0.1;
+  if (noScale < 0.3) noScale = 0.3;
+  noBtn.style.transform = `scale(${noScale})`;
+
+  // YES GROSSIT
+  yesScale += 0.1;
+  if (yesScale > 2.5) yesScale = 2.5;
+  yesBtn.style.transform = `scale(${yesScale})`;
+});
+
+// Animation bras ouverts
+function openArms() {
+  document.getElementById("armLeft").setAttribute("x2", 0);
+  document.getElementById("armRight").setAttribute("x2", 200);
+}
+
+
+// Clic YES
+yesBtn.addEventListener("click", () => {
+  openArms();
+  loveText.style.display = "block";
+  createHearts();
+});
+
+
+// Pluie de cœurs
+function createHearts() {
+  for (let i = 0; i < 40; i++) {
+    const heart = document.createElement("div");
+    heart.innerHTML = "❤️";
+    heart.style.position = "absolute";
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    heart.style.top = window.innerHeight + "px";
+    heart.style.fontSize = "24px";
+    document.body.appendChild(heart);
+
+    heart.animate(
+      [
+        { transform: "translateY(0)", opacity: 1 },
+        { transform: "translateY(-800px)", opacity: 0 }
+      ],
+      { duration: 3000 }
+    );
+
+    setTimeout(() => heart.remove(), 3000);
+  }
+}
